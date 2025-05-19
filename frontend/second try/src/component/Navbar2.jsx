84ea3db3fc876a55265f21038df0d3cfd7e420dd
samay <samayverma44@gmail.com>
2025-05-19 23:06:15 +0530

    navigation bar is ready

--- frontend/second try/src/component/Navbar2.jsx
+++ frontend/second try/src/component/Navbar2.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
-
+import dataImage from "../assets/data.svg"
 function Navbar() {
   return (
-    <div>
-      <h1 class='text-red-900'>hello</h1>
-      <p class="text-blue-600/100 dark:text-sky-400/100">The quick brown fox...</p>
+    <div className='fixed z-[999] w-full px-10 py-5 flex justify-between items-center'>
+      <img src={dataImage} alt="Data"  className='w-13'/>
+      <h1 class='text-red-900'></h1>
+    <div className='flex gap-10 text-amber-200 font-sans ... '>
+      {['Services','Our work','About us','Insights','Contect us'].map((iteam,index)=>(
+<a key={index} className={` text-lg ${index===4 && "ml-32"}`}>{iteam}</a>
+      ))}
+    </div>
 
     </div>
   )
